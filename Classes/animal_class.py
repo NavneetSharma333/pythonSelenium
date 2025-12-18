26d7fcebcef485f7535c5c109046975d378c126b
NavneetSharma333 <navneet240408@gmail.com>
2025-12-18 23:05:31 +0530

    addedClasses

---
+++ Classes/animal_class.py
@@ -0,0 +1,28 @@
+
+
+class animal:
+    
+    def __init__(self, color="red", food_type="none"):
+        print("This is init.")
+        self.rang = color
+        self.food = food_type
+        
+    def move(self):
+        print("animal moves")
+        
+    def eat(self):
+        print("animal eats when-ever required")
+        print("this animal eats {}". format(self.food))
+        
+    def breath(self):
+        print("animal can breathe")
+        
+        
+my_animal_1 = animal("white", "Veggies")
+print(f"color of animal 1: {my_animal_1.rang}")
+my_animal_1.move()
+my_animal_1.eat()
+
+
+my_animal_2 = animal("Brown", "non-veg")
+my_animal_2.eat()
