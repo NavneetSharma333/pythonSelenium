8f3997f100f2ae12a541bbe9b0add55e541b6611
NavneetSharma333 <navneet240408@gmail.com>
2025-12-21 20:47:45 +0530

    InheritanceWithParentClass

--- Classes/inheritance_with_animal_class.py
+++ Classes/inheritance_with_animal_class.py
@@ -1,16 +1,29 @@
 
 
-class Dog():
+class Animal:
     
-    def __init__(self, breed, name):
-        self.dog_breed = breed
-        self.dog_name = name
+    def __init__(self, color, food_type):
+        print("This is init from Parent.")
+        self.rang = color
+        self.food = food_type
+        
+    def move(self):
+        print("animal moves")
         
     def eat(self):
-        print("eat.........")
+        print("animal eats when-ever required")
+        print("this animal eats {}". format(self.food))
         
     def breath(self):
-        print("breath........")
+        print("animal can breathe")
+
+
+class Dog(Animal):
+    
+    def __init__(self, color, food_type, breed, name):
+        super().__init__(color, food_type)
+        self.dog_name = name
+        self.dog_breed = breed
         
     def bark(self):
         print("wooff")
@@ -20,30 +33,30 @@ class Dog():
         
         
         
-class Cat():
+class Cat(Animal):
     
-    def __init__(self, breed, name):
+    def __init__(self, color, food_type, breed, name):
+        super().__init__(color, food_type)
         self.cat_breed = breed
         self.cat_name = name
-        
-    def eat(self):
-        print("Eat.....")
-        
-    def breath(self):
-        print("cat breath....")
-        
+
     def meaw(self):
         print("Meaww")
         
     
-myDog = Dog("kiran", "kalu")
-print(myDog.dog_breed)
+myDog = Dog("Black", "meat", "rotwheeler", "kalu")
 print(myDog.dog_name)
+print(myDog.dog_breed)
+print(myDog.rang)
 myDog.eat()
 myDog.bark()
 myDog.breath()
 
 
-myCat = Cat("white", "catu")
-myCat.eat()
+
+myCat = Cat("white", "chawal", "persian", "catu")
+print(myCat.cat_name)
+print(myCat.cat_breed)
+print(myCat.rang)
 myCat.meaw()
+myCat.eat()
