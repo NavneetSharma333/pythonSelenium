142adc8a12c78bd7f63ebe1a12d3dbdd3493127c
NavneetSharma333 <navneet240408@gmail.com>
2025-12-24 23:07:43 +0530

    AddedNewmethodInClass

--- Classes/inheritance_with_animal_class.py
+++ Classes/inheritance_with_animal_class.py
@@ -2,10 +2,12 @@
 
 class Animal:
     
-    def __init__(self, color, food_type):
+    def __init__(self, color, food_type, playing, name):
         print("This is init from Parent.")
         self.rang = color
         self.food = food_type
+        self.playing = playing
+        self.name = name
         
     def move(self):
         print("animal moves")
@@ -16,12 +18,14 @@ class Animal:
         
     def breath(self):
         print("animal can breathe")
-
+        
+    def playing_with_kids(self):
+        print(f"{self.name} is friendly with everyone and play {self.playing}")     
 
 class Dog(Animal):
     
-    def __init__(self, color, food_type, breed, name):
-        super().__init__(color, food_type)
+    def __init__(self, color, food_type, breed, name, playing):
+        super().__init__(color, food_type, playing, name)
         self.dog_name = name
         self.dog_breed = breed
         
@@ -35,8 +39,8 @@ class Dog(Animal):
         
 class Cat(Animal):
     
-    def __init__(self, color, food_type, breed, name):
-        super().__init__(color, food_type)
+    def __init__(self, color, food_type, breed, name, playing):
+        super().__init__(color, food_type, playing, name)
         self.cat_breed = breed
         self.cat_name = name
 
@@ -44,19 +48,20 @@ class Cat(Animal):
         print("Meaww")
         
     
-myDog = Dog("Black", "meat", "rotwheeler", "kalu")
+myDog = Dog("Black", "meat", "rotwheeler", "Dog-kalu", "flying disc")
 print(myDog.dog_name)
 print(myDog.dog_breed)
 print(myDog.rang)
 myDog.eat()
 myDog.bark()
 myDog.breath()
+myDog.playing_with_kids()
 
 
-
-myCat = Cat("white", "chawal", "persian", "catu")
+myCat = Cat("white", "chawal", "persian", "Cat-Kitty", "jumping on tree")
 print(myCat.cat_name)
 print(myCat.cat_breed)
 print(myCat.rang)
 myCat.meaw()
 myCat.eat()
+myCat.playing_with_kids()
